55001e9c27ad90f71b17ed01a510bbde6a619ea0
Dom <39652233+domingodavid@users.noreply.github.com>
2021-10-11 08:06:11 -0700

    Initial Commit and Push

---
+++ Primes/src/Driver.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+	   
+        final int MAX = 100;
+        boolean primes[];
+        primes = new boolean[MAX];
+        compute(primes);
+        display(primes);
+	}
+
+	/* Use the algorithm described in the lab description so that the array, nums, contains all the
+	 * valid primes. All others should be -1.
+	 */
+	public static void compute(boolean[] nums) {
+ 
+	}
+	
+	/*
+	 * Complete the method below so that it prints all the prime numbers in the integer array nums
+	 * Assume that the compute method has been called that sets the array to contain the prime numbers as
+	 * described in the lab document.
+	 */
+	public static void display(boolean[] nums) {
+
+	
+	}
+	
+}
